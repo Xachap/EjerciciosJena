78baaf71f821d46bc9e049cd70b8f9b5598112c5
Jesús Pérez <jesus.perez15@alu.uclm.es>
2019-01-21 18:24:07 +0100

    Add files via upload

--- JenaEjercicios/Ejercicio6.java
+++ JenaEjercicios/Ejercicio6.java
@@ -2,9 +2,11 @@ package JenaEjercicios;
 
 import java.io.InputStream;
 
+import org.apache.jena.ontology.Individual;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.OntProperty;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
@@ -28,7 +30,7 @@ import org.apache.jena.vocabulary.VCARD;
 public class Ejercicio6 {
 	
 	// EJERCICIO 6: Modificando ontologías (RDFs)
-	
+	static final String NS = "http://somewhere#";
 	public static void main (String[] args){
 		
 		String filename = "example5.rdf";
@@ -47,20 +49,30 @@ public class Ejercicio6 {
 		model.read(in, null);
 		
 		// Creamos una nueva clase llamada "Researcher"
-		OntClass researcher = model.createClass(RDFS.getURI()+"Researcher");
+		OntClass researcher = model.createClass(NS+"Researcher");
 		
 		// ** TAREA 6.1: Crear una nueva clase llamada "University" **
-		OntClass university = model.createClass(RDFS.getURI()+"University");
+		OntClass university = model.createClass(NS+"University");
 		
 		// ** TAREA 6.2: Añadir "Researcher" como subclase de "Person" **
+		OntClass person = model.getOntClass(NS+"Person");
+		person.addSubClass(researcher);
 		
 		// ** TAREA 6.3: Crear una nueva object property llamada "workIn" **
+		Property workIn = model.createProperty(NS+"workIn");
 		
 		// ** TAREA 6.4: Crear una nueva instancia de Researcher llamada "Jane Smith" **
+		Individual janeSmith = researcher.createIndividual(NS+"JaneSmith");
 		
 		// ** TAREA 6.5: Añadir a la instancia JaneSmith su fullName, given name y family name **
+		janeSmith.addLiteral(VCARD.FN, "Jane Smith");
+		janeSmith.addLiteral(VCARD.Given, "Jane");
+		janeSmith.addLiteral(VCARD.Family, "Smith");
 		
 		// ** TAREA 6.6: Añadir UPM como la universidad donde John Smith trabaja **
+		Individual upm = university.createIndividual(NS+"UPM");
+		Resource johnSmith = model.getResource(NS+"JohnSmith");
+		johnSmith.addProperty(workIn, NS+"UPM");
 		
 		model.write(System.out, "RDF/XML-ABBREV");
 		//
